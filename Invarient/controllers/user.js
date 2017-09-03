dd692354bacbd2e335f557396ce8624526657fe5
Alex Wilf <abwilf@umich.edu>
2017-09-03 14:33:09 -0400

    updated forgot password link

--- Invarient/controllers/user.js
+++ Invarient/controllers/user.js
@@ -449,52 +449,58 @@ exports.postForgot = (req, res, next) => {
     return res.redirect('/forgot');
   }
 
-  const createRandomToken = crypto
-    .randomBytesAsync(16)
-    .then(buf => buf.toString('hex'));
+  // DELETE THIS WHEN IMPLEMENT FORGOT PASSWORD
+  else {
+      req.flash('info', { msg: "This functionality is currently not supported.  Please reach out to us in the 'Contact Us' section for help at this time!" });
+      return res.redirect('/forgot');
+  }
+  ////////////////////////////////////////////////////////////////////////
+  // const createRandomToken = crypto
+  //   .randomBytesAsync(16)
+  //   .then(buf => buf.toString('hex'));
 
-  const setRandomToken = token =>
-    User
-      .findOne({ email: req.body.email })
-      .then((user) => {
-        if (!user) {
-          req.flash('errors', { msg: 'Account with that email address does not exist.' });
-        } else {
-          user.passwordResetToken = token;
-          user.passwordResetExpires = Date.now() + 3600000; // 1 hour
-          user = user.save();
-        }
-        return user;
-      });
+  // const setRandomToken = token =>
+  //   User
+  //     .findOne({ email: req.body.email })
+  //     .then((user) => {
+  //       if (!user) {
+  //         req.flash('errors', { msg: 'Account with that email address does not exist.' });
+  //       } else {
+  //         user.passwordResetToken = token;
+  //         user.passwordResetExpires = Date.now() + 3600000; // 1 hour
+  //         user = user.save();
+  //       }
+  //       return user;
+  //     });
 
-  const sendForgotPasswordEmail = (user) => {
-    if (!user) { return; }
-    const token = user.passwordResetToken;
-    const transporter = nodemailer.createTransport({
-      service: 'SendGrid',
-      auth: {
-        user: process.env.SENDGRID_USER,
-        pass: process.env.SENDGRID_PASSWORD
-      }
-    });
-    const mailOptions = {
-      to: user.email,
-      from: 'hackathon@starter.com',
-      subject: 'Reset your password on Hackathon Starter',
-      text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
-        Please click on the following link, or paste this into your browser to complete the process:\n\n
-        http://${req.headers.host}/reset/${token}\n\n
-        If you did not request this, please ignore this email and your password will remain unchanged.\n`
-    };
-    return transporter.sendMail(mailOptions)
-      .then(() => {
-        req.flash('info', { msg: `An e-mail has been sent to ${user.email} with further instructions.` });
-      });
-  };
+  // const sendForgotPasswordEmail = (user) => {
+  //   if (!user) { return; }
+  //   const token = user.passwordResetToken;
+  //   const transporter = nodemailer.createTransport({
+  //     service: 'SendGrid',
+  //     auth: {
+  //       user: process.env.SENDGRID_USER,
+  //       pass: process.env.SENDGRID_PASSWORD
+  //     }
+  //   });
+  //   const mailOptions = {
+  //     to: user.email,
+  //     from: 'hackathon@starter.com',
+  //     subject: 'Reset your password on Hackathon Starter',
+  //     text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
+  //       Please click on the following link, or paste this into your browser to complete the process:\n\n
+  //       http://${req.headers.host}/reset/${token}\n\n
+  //       If you did not request this, please ignore this email and your password will remain unchanged.\n`
+  //   };
+  //   return transporter.sendMail(mailOptions)
+  //     .then(() => {
+  //       req.flash('info', { msg: `An e-mail has been sent to ${user.email} with further instructions.` });
+  //     });
+  // };
 
-  createRandomToken
-    .then(setRandomToken)
-    .then(sendForgotPasswordEmail)
-    .then(() => res.redirect('/forgot'))
-    .catch(next);
+  // createRandomToken
+  //   .then(setRandomToken)
+  //   .then(sendForgotPasswordEmail)
+  //   .then(() => res.redirect('/forgot'))
+  //   .catch(next);
 };
