61364abdfce60fffbb8be6aa2ccf0af4663ffebf
Alex Wilf <abwilf@umich.edu>
2017-09-03 21:57:41 -0400

    server side svg support

--- Invarient/app.js
+++ Invarient/app.js
@@ -113,6 +113,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+app.use(express.static('public'));
 
 /**
  * Primary app routes.
